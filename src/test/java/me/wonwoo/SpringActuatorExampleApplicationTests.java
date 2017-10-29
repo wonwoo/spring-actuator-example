9b8ac4d9a2b08ef47cc753ecb2b54c4ce4509b1b
wonwoo <aoruqjfu@gmail.com>
2017-10-29 23:16:45 +0900

    remove

--- src/test/java/me/wonwoo/SpringActuatorExampleApplicationTests.java
+++ src/test/java/me/wonwoo/SpringActuatorExampleApplicationTests.java
@@ -5,9 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringActuatorExampleApplicationTests {
