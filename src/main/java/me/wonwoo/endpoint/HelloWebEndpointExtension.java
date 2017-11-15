de76ccc6593fa799b9ff657a6bf86ef8b08d184f
wonwoo <aoruqjfu@gmail.com>
2017-11-15 22:17:47 +0900

    post body

--- src/main/java/me/wonwoo/endpoint/HelloWebEndpointExtension.java
+++ src/main/java/me/wonwoo/endpoint/HelloWebEndpointExtension.java
@@ -2,6 +2,7 @@ package me.wonwoo.endpoint;
 
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
 import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpointExtension;
 import org.springframework.lang.Nullable;
@@ -23,5 +24,27 @@ public class HelloWebEndpointExtension {
   @ReadOperation
   public WebEndpointResponse<String> selector(@Selector String name) {
     return new WebEndpointResponse<>(delegate.hello(name));
+  }
+
+  @WriteOperation
+  public WebEndpointResponse<Person> person(String name) {
+    return new WebEndpointResponse<>(new Person(delegate.hello(name)));
+  }
+
+
+  static class Person {
+    private String name;
+
+    public Person(String name) {
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 }
