128568427fede2f851f3c14304880f06256bd8eb
wonwoo <aoruqjfu@gmail.com>
2017-11-15 22:10:46 +0900

    Selector

--- src/main/java/me/wonwoo/endpoint/HelloWebEndpointExtension.java
+++ src/main/java/me/wonwoo/endpoint/HelloWebEndpointExtension.java
@@ -1,6 +1,7 @@
 package me.wonwoo.endpoint;
 
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
 import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpointExtension;
 import org.springframework.lang.Nullable;
@@ -16,6 +17,11 @@ public class HelloWebEndpointExtension {
 
   @ReadOperation
   public WebEndpointResponse<String> hello(@Nullable String name) {
+    return new WebEndpointResponse<>(delegate.hello(name));
+  }
+
+  @ReadOperation
+  public WebEndpointResponse<String> selector(@Selector String name) {
     return new WebEndpointResponse<>(delegate.hello(name));
   }
 }
